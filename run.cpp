135634f804caa04f0d7693459bea420ca3471b8a
shambhavisud <67536519+shambhavisud@users.noreply.github.com>
2021-12-21 08:40:33 +0530

    final changes

--- run.cpp
+++ run.cpp
@@ -1,103 +1,87 @@
-// CPP Program to generate random alphabets
+
 #include <bits/stdc++.h>
 #include <iostream>
 #include <fstream>
 using namespace std;
 
-const int MAX = 4;
+const int soize = 4;
 int indel, sub, L, D;
 
-int possible_substring(string str, int length, int n)
+int editDistance(string start, string target)
 {
-    int count = length - n + 1;
-    return count;
-}
+    int m = start.length();
+    int n = target.length();
 
-// Returns a string of random alphabets of
-// length n.
-string printRandomString(int n)
-{
-    char alphabet[MAX] = {'A', 'C', 'G', 'T'};
+    // For every pair of i and j, E[i,j] stores the Edit distance
+    // b/w the 1st `i` characters of `start` & the first `j` characters of target string.
+    // Matrix E will hold (m+1)*(n+1) vals
+    int E[m + 1][n + 1];
 
-    string res = "";
-    for (int i = 0; i < n; i++)
-        res = res + alphabet[rand() % MAX];
+    // initialize `T` by all 0's
+    memset(E, 0, sizeof E);
 
-    return res;
-}
+    // we can transform source prefixes into an empty string by
+    // dropping all characters
 
-int editDistance(string s1, string s2)
-{
-
-    int m = s1.size();
-    int n = s2.size();
-
-    int **E = new int *[s1.size() + 1];
-
-    for (int i = 0; i < s1.size() + 1; i++)
+    for (int i = 1; i <= m; i++)
     {
-        E[i] = new int[s2.size() + 1];
+        E[i][0] = i; // (case i)
     }
 
-    E[0][0] = 0;
+    // Reaching the target prefixes from empty start prefix
+    // through insertion of ev char
 
-    for (int i = 1; i < m + 1; i++)
+    for (int j = 1; j <= n; j++)
     {
-        E[i][0] = i;
-    }
-    for (int i = 1; i < n + 1; i++)
-    {
-        E[0][i] = i;
+        E[0][j] = j; // (case 1)
     }
 
-    for (int i = 1; i < m + 1; i++)
-    {
+    int sub_cost;
 
-        for (int j = 1; j < n + 1; j++)
+    // Bottoms up filling of Matrix E
+    for (int i = 1; i <= m; i++)
+    {
+        for (int j = 1; j <= n; j++)
         {
-            if (s1[m - i] == s2[n - j])
-                E[i][j] = E[i - 1][j - 1];
-
+            if (start[i - 1] == target[j - 1])
+            { // (case ii)
+                sub_cost = 0;
+            }
             else
             {
-                E[i][j] = min(indel + E[i][j - 1], min(indel + E[i - 1][j], sub + E[i - 1][j - 1]));
+                sub_cost = sub; // (case iii(3))
             }
+            E[i][j] = min(min(E[i - 1][j] + indel,     // delete (case iii(2))
+                              E[i][j - 1] + indel),    // insert (case iii(1))
+                          E[i - 1][j - 1] + sub_cost); // substitute (case ii and iii(3))
         }
     }
 
     return E[m][n];
 }
 
-void print_neighbour(string A[], string s)
+int num_of_subs_possible(string str, int length, int n)
 {
-    // ofstream outfile;
-    // outfile.open("output.txt");
-    int flag = 0;
-
-    for (int i = 0; i < 20; i++)
-    {
-        if (editDistance(s, A[i]) < D || editDistance(s, A[i]) == D)
-        {
-            flag = 1;
-        }
-        else
-        {
-            flag = 0;
-            break;
-        }
-    }
-    if (flag == 0)
-    {
-        //outfile << s << " is a neighbour to all others!\n";
-    }
+    int num_subs = length - n + 1;
+    return num_subs;
 }
 
-// Driver code
+string randomStringGenerator(int n)
+{
+    char set_acgt[soize] = {'A', 'C', 'G', 'T'};
+
+    string gen_str = "";
+    for (int i = 0; i < n; i++)
+        gen_str = gen_str + set_acgt[rand() % soize];
+
+    return gen_str;
+}
+
 int main()
 {
     ofstream outfile;
     ifstream infile;
-    infile.open("input.txt");
+    infile.open("Data.txt");
     int inputArray[4];
     int cnt = 0;
     int read_num;
@@ -106,68 +90,65 @@ int main()
         inputArray[cnt++] = read_num;
     }
 
-    // cout << "The integers are:"
-    //      << "\n";
-    // for (int i = 0; i < cnt; i++)
-    // {
-    //     cout << inputArray[i] << ' ';
-    // }
-
     D = inputArray[0];
     L = inputArray[1];
     indel = inputArray[2];
     sub = inputArray[3];
 
-    outfile.open("output.txt");
+    outfile.open("Out.txt");
 
     srand(time(NULL));
     int n = 600;
     string A[20];
     for (int i = 0; i < 20; i++)
     {
-        A[i] = printRandomString(n);
+        A[i] = randomStringGenerator(n);
     }
 
-    // cout << "enter values of D L indel and sub\n";
-    // cin >> D >> L >> indel >> sub;
-
-    // for (int i = 0; i < 20; i++)
-    // {
-    //     outfile << A[i] << "\n";
-    // }
-
-    //outfile << "Count of substrings of length n possible from the given string are: " << possible_substring(str, length, L);
-
-    for (int i = 0; i < 20; i++) //for each string 1 to 20
+    for (int i = 0; i < 20; i++) // for each string 1 to 20
     {
         string str = A[i];
 
         int length = str.length();
-        string sub_string[possible_substring(str, length, L)]; //initialise array size with possible number of susbstrings
-        for (int j = 0; j < A[i].length() - L + 1; j++)        //initialise the array with all possible substrings
-        {
+        string sub_string[num_of_subs_possible(str, length, L)]; // initialise array size with possible number of susbstrings
+        for (int j = 0; j < A[i].length() - L + 1; j++)          // for each substring of A[i]
+        {                                                        // initialise the array with all possible substrings
 
             sub_string[j] = A[i].substr(j, L);
+
+            // int k;
             int flag = 0;
 
-            //print_neighbour(A, sub_string[j]);
-            int k;
-            for (k = 0; k < 20; k++)
+            for (int k = 0; k < 20; k++)
             {
 
-                if (editDistance(sub_string[j], A[k]) < D || editDistance(sub_string[j], A[k]) == D)
+                if (k == i)
                 {
-                    flag = 1;
+                    continue;
                 }
                 else
                 {
-                    flag = 0;
-                    break;
+                    string other_sub_string[num_of_subs_possible(A[k], A[k].length(), L)];
+                    for (int y = 0; y < A[k].length() - L + 1; y++)
+                    {
+
+                        other_sub_string[y] = A[k].substr(y, L);
+                        int eds = editDistance(sub_string[j], other_sub_string[y]);
+
+                        if (eds < D || eds == D)
+                        {
+
+                            flag++;
+
+                            break;
+                        }
+                    }
                 }
             }
-            if (flag == 0)
+
+            if (flag == 19)
             {
-                outfile << sub_string[j] << "ed is " << editDistance(sub_string[j], A[k]) << "\n";
+                outfile << sub_string[j] << "\n";
             }
         }
     }
